d2eb57e0a2eb2852dc16c41efad96efb6b4b3a5d
M Khan <khan@vsparrow.com>
2017-05-16 13:19:54 -0400

    refactor code from operand to flushCurrentValueString

--- js/index.js
+++ js/index.js
@@ -13,8 +13,8 @@
 // periodHandler						will forward period to updateCurrentValueString if one hasn't been used
 //											in the current operand.
 // clearEntry							sets currentValueString to 0. Sets periodAllowed to true 
-// operand 								pushes currentValueString and operator to array valueChain
-//										
+// operand 								pushes operator to array valueChain
+// flushCurrentValueString				push currentValueString to array valueChain						
 //************************************************************************************
 //************************************************************************************
 //Functions tested are in this block
@@ -28,7 +28,7 @@ var operandAllowed = true;
 function multiply(operand1,operand2)	{return operand1 * operand2};
 function divide(operand1,operand2)		{return operand1 / operand2};
 function minus(operand1,operand2)		{return operand1 - operand2};
-function plus(operand1,operand2)		{return operand1 + operand2};
+function plus(operand1,operand2)		{return Number(operand1) + Number(operand2)};
 
 function updateCurrentValueString(numberString){
 	if(currentValueString === "0") {
@@ -51,23 +51,70 @@ function clearEntry(){
 
 function operand(op){
 	if(operandAllowed){
-		valueChain.push(currentValueString);
+		flushCurrentValueString();
 		valueChain.push(op);
+		console.log("valueChain :" + valueChain);	
+	}			
+}
+
+function flushCurrentValueString(){
+		valueChain.push(currentValueString);  //see operand for duplicate code
 		currentValueString = "0";
 		periodAllowed = true;
-		console.log("valueChain :" + valueChain);	
-		operandAllowed = false;
-	}			
+		operandAllowed = false;	
 }
 //************************************************************************************
 //************************************************************************************
 
 
+var total = 0;
+
+function evaluateChain(){
+	// if( !isOperator) {}//if
+	var evalTotal = null;
+	while(valueChain.length > 0){
+
+		flushCurrentValueString();
+		// valueChain.push(currentValueString);  //see operand for duplicate code
+		// currentValueString = "0";
+		// periodAllowed = true;
+		// operandAllowed = false;
 
 
+		var shifted = valueChain.shift();
+		if(evalTotal === null){evalTotal = shifted}
+		else{
+			var operatorValue = shifted;
+			shifted = valueChain.shift();
+			// evalTotal operator shifted
+			// evalTotal operator shifted
+			console.log("evalTotal,operatorValue, shifted" + evalTotal + " " +operatorValue + " "+  shifted);
 
-//found that after operand is called, when we add "0" and "." we get "." not "0." 
+			console.log("valueChain",valueChain)
+			switch(operatorValue){
+			  case "X": evalTotal=multiply(evalTotal,shifted); break;
+		      case "/": evalTotal=divide(evalTotal,shifted); break;  
+		      case "-": evalTotal=minus(evalTotal,shifted); break;  
+		      case "plus": evalTotal=plus(evalTotal,shifted); break;
+		      
+            }//switch}
+            console.log("evalTotal is:", evalTotal)  	
+	    }    
+	}//w
+	console.log("TOTAL: " + evalTotal)
+}//fun
 
+
+function isOperator(val){
+	// if(	   valueChain[length-1] === "+" 
+	// 	|| valueChain[length-1] === "-" 
+	// 	|| valueChain[length-1] === "X" 
+	// 	|| valueChain[length-1] === "/") 
+	if( val === "plus"  || val === "-" || val === "X" || val === "/" ) {return true}
+	return false;	
+}
+
+ 
 //************************************************************************************
 //************************************************************************************
 
@@ -77,115 +124,115 @@ var currentElm="0";
 // ****************************************************
 
 
-function equalsEvaluator(chain){   //when equal is pressed it then asssits in  the evaulation of the input
-  //alert("This is currentChain that was passed:"+chain);  
+// function equalsEvaluator(chain){   //when equal is pressed it then asssits in  the evaulation of the input
+//   //alert("This is currentChain that was passed:"+chain);  
   
-  while(chain.length>1){
-    var operand1 = chain[0];
-    var operator = chain[1];
-    var operand2 = chain[2];
-    var evaluation = "";
-    chain.splice(0,3);
-    //alert("operand1:" + operand1 + " operator:" + operator + " operand2:" + operand2);
-    //alert("chain after splice: " + chain)
-    switch(operator){
-      case "X": evaluation=multiply(operand1,operand2); break;
-      case "/": evaluation=divide(operand1,operand2); break;  
-      case "-": evaluation=minus(operand1,operand2); break;  
-      case "+": evaluation=plus(operand1,operand2); break;  
-                   }//switch
-    //alert("evaluation:"+evaluation)
-    chain.unshift(evaluation)
-    //alert("chain after unshift::"+ chain)
-  }
-  currentElm="0";
-  currentChain=["0"];
-  document.getElementById("answer").innerText=chain[0];
-  document.getElementById("answer").style.visibility="visible";
-  document.getElementById("currentElm").style.visibility="hidden";
-} //equalsEvaluator
+//   while(chain.length>1){
+//     var operand1 = chain[0];
+//     var operator = chain[1];
+//     var operand2 = chain[2];
+//     var evaluation = "";
+//     chain.splice(0,3);
+//     //alert("operand1:" + operand1 + " operator:" + operator + " operand2:" + operand2);
+//     //alert("chain after splice: " + chain)
+//     switch(operator){
+//       case "X": evaluation=multiply(operand1,operand2); break;
+//       case "/": evaluation=divide(operand1,operand2); break;  
+//       case "-": evaluation=minus(operand1,operand2); break;  
+//       case "+": evaluation=plus(operand1,operand2); break;  
+//                    }//switch
+//     //alert("evaluation:"+evaluation)
+//     chain.unshift(evaluation)
+//     //alert("chain after unshift::"+ chain)
+//   }
+//   currentElm="0";
+//   currentChain=["0"];
+//   document.getElementById("answer").innerText=chain[0];
+//   document.getElementById("answer").style.visibility="visible";
+//   document.getElementById("currentElm").style.visibility="hidden";
+// } //equalsEvaluator
 
-function operate(operator){
-  if(currentChain[0]!== "0") {currentChain.push(currentElm);}
-  else {currentChain[0] = currentElm;}
-  currentElm = operator;
-}
+// function operate(operator){
+//   if(currentChain[0]!== "0") {currentChain.push(currentElm);}
+//   else {currentChain[0] = currentElm;}
+//   currentElm = operator;
+// }
 
-// ****************************************************
-// Main function
-/*
-function update(value){
+// // ****************************************************
+// // Main function
+
+// function update(value){
   
-  document.getElementById("currentElm").style.visibility="visible";
-  document.getElementById("answer").style.visibility="hidden";
-  if(value=='AC') {currentElm = "0"; currentChain=["0"];}
-  if(value=='CE') {currentElm = "0";}
-  if(value >=0 && value <=9) 
-    { 
-      if( currentElm == "X" ||  currentElm == "/" ||  currentElm == "+"  ||  currentElm == "-" ){
-        currentChain.push(currentElm); currentElm="";
-      }
-      if (currentElm == "0" && currentElm != "0.") { currentElm = value.toString()}  //if currentElm was 0
-              //checkCurrentLength, will allow continue if less that 8 digits.
-      else { if(checkCurrentElmLength()) {  currentElm += value.toString();} }    // else add the number to end of currentElm
+//   document.getElementById("currentElm").style.visibility="visible";
+//   document.getElementById("answer").style.visibility="hidden";
+//   if(value=='AC') {currentElm = "0"; currentChain=["0"];}
+//   if(value=='CE') {currentElm = "0";}
+//   if(value >=0 && value <=9) 
+//     { 
+//       if( currentElm == "X" ||  currentElm == "/" ||  currentElm == "+"  ||  currentElm == "-" ){
+//         currentChain.push(currentElm); currentElm="";
+//       }
+//       if (currentElm == "0" && currentElm != "0.") { currentElm = value.toString()}  //if currentElm was 0
+//               //checkCurrentLength, will allow continue if less that 8 digits.
+//       else { if(checkCurrentElmLength()) {  currentElm += value.toString();} }    // else add the number to end of currentElm
               
-    }
-  if(value=='.') 
-    {
-      if(currentElm.indexOf('.')>= 0) {} //do nothing
-      else if(currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+')  {operate(value)}//{currentChain.push(currentElm);currentElm += '.';} 
-      else {currentElm += '.';}
-    }
-  if(value == 'X' || value == '/' || value == '-' || value == '+'){
-    if(Number(currentElm) == 0 || currentElm == "0." || currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+') {} //do nothing
-    else { //alert(currentElm); 
-           operate(value);
-         }  // *** DO SOME KIND OF UPDATE
-    } // value is operator except =
-  if(value == "=") 
-    { if(currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+') {} //do nothing
-      else{
-        //alert("CONTINUE HERE , see what equals will do");
-        currentChain.push(currentElm);
-        equalsEvaluator(currentChain);
-      } //else      
-    }//if value == "="
+//     }
+//   if(value=='.') 
+//     {
+//       if(currentElm.indexOf('.')>= 0) {} //do nothing
+//       else if(currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+')  {operate(value)}//{currentChain.push(currentElm);currentElm += '.';} 
+//       else {currentElm += '.';}
+//     }
+//   if(value == 'X' || value == '/' || value == '-' || value == '+'){
+//     if(Number(currentElm) == 0 || currentElm == "0." || currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+') {} //do nothing
+//     else { //alert(currentElm); 
+//            operate(value);
+//          }  // *** DO SOME KIND OF UPDATE
+//     } // value is operator except =
+//   if(value == "=") 
+//     { if(currentElm == 'X' || currentElm == '/' || currentElm == '-' || currentElm == '+') {} //do nothing
+//       else{
+//         //alert("CONTINUE HERE , see what equals will do");
+//         currentChain.push(currentElm);
+//         equalsEvaluator(currentChain);
+//       } //else      
+//     }//if value == "="
   
-  displayCurrentElm(currentElm);
-  displayCurrentChain(currentChain);
-}
-*/
-// ****************************************************
+//   displayCurrentElm(currentElm);
+//   displayCurrentChain(currentChain);
+// }
+
+// // ****************************************************
 
 
-// *****************************************************
-// DISPLAY FUNCTIONS
-function displayCurrentElm(toDisplay){   //display current number or operation in h2 currentElm
-  document.getElementById("currentElm").innerText = toDisplay;  
-}
+// // *****************************************************
+// // DISPLAY FUNCTIONS
+// function displayCurrentElm(toDisplay){   //display current number or operation in h2 currentElm
+//   document.getElementById("currentElm").innerText = toDisplay;  
+// }
 
-function displayCurrentChain(toDisplayChain){  //display current chain in h4 currentChain
-  var text = "";
-  for(i=0;i<toDisplayChain.length;i++){
-    text += toDisplayChain[i]
-  }  
-  document.getElementById("currentChain").innerText = text;
-}
-// END DISPLAY ********************************************
+// function displayCurrentChain(toDisplayChain){  //display current chain in h4 currentChain
+//   var text = "";
+//   for(i=0;i<toDisplayChain.length;i++){
+//     text += toDisplayChain[i]
+//   }  
+//   document.getElementById("currentChain").innerText = text;
+// }
+// // END DISPLAY ********************************************
 
-// ********************************************************
-// CHECK FUNTIONS
-function checkCurrentElmLength(){     //returns true if length < 8 else returns false
-  var tempString = currentElm.replace(/\./g,'');
-  //alert(tempString);
-  if (tempString.length >8) {alert("ERROR MORE THAN 8 DIGITS, CANNOT CONTINUE")}
-  return tempString.length <=8;
-}
+// // ********************************************************
+// // CHECK FUNTIONS
+// function checkCurrentElmLength(){     //returns true if length < 8 else returns false
+//   var tempString = currentElm.replace(/\./g,'');
+//   //alert(tempString);
+//   if (tempString.length >8) {alert("ERROR MORE THAN 8 DIGITS, CANNOT CONTINUE")}
+//   return tempString.length <=8;
+// }
 
-// END CHECK **********************************************
+// // END CHECK **********************************************
 
-// ********************************************************
-// START PROGRAM ******************************************
+// // ********************************************************
+// // START PROGRAM ******************************************
 
-displayCurrentElm(currentElm);
-displayCurrentChain(currentChain);
+// displayCurrentElm(currentElm);
+// displayCurrentChain(currentChain);
